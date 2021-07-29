ec8ef257c6d3f5104a2c518f159c5f1265d89480
hogang1223 <hyogangster1@gmail.com>
2021-07-30 02:13:06 +0900

    drawing login and sign up page

---
+++ BabyProject/BabyProject/DBModel/UserDBModel.swift
@@ -0,0 +1,38 @@
+//
+//  UserDBModel.swift
+//  BabyProject
+//
+//  Created by hyogang on 2021/07/30.
+//
+
+import Foundation
+
+class UserDBModel: NSObject{
+    
+    var usercode : String?
+    var email : String?
+    var password : String?
+    var nickname : String?
+    var phone : String?
+    var babyage : String?
+    var rating : String?
+    var signupdate : String?
+    var signoutdate : String?
+    
+    override init() {
+    }
+
+    
+    init(usercode: String, email: String, password: String, nickname: String, phone: String, babyage: String, rating: String, signupdate: String, signoutdate: String){
+        self.usercode = usercode
+        self.email = email
+        self.password = password
+        self.nickname = nickname
+        self.phone = phone
+        self.babyage = babyage
+        self.rating = rating
+        self.signupdate = signupdate
+        self.signoutdate = signoutdate
+    }
+
+}
---
+++ BabyProject/BabyProject/HK/FindPasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FindPasswordViewController.swift
+//  BabyProject
+//
+//  Created by hyogang on 2021/07/30.
+//
+
+import UIKit
+
+class FindPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
