7fe21751f753c008d1ebedff6fe2c66201859656
TJ <osa070@knou.ac.kr>
2021-08-02 17:28:13 +0900

    0802 상품등록란 추가, Model추가
    0802 상품등록란 추가, Model추가

---
+++ BabyProject/BabyProject/DBModel/ItemInsertModel.swift
@@ -0,0 +1,45 @@
+//
+//  ItemInsertModel.swift
+//  BabyProject
+//
+//  Created by TJ on 2021/08/02.
+//
+
+import Foundation
+
+// Insert, Update, Delete할 때에는 protocol이 필요 X
+
+class ItemInsertModel {
+    var share = Share()
+    var urlPath = "http://192.168.0.127:8080/bebeProject/ItemAddInsert_ios.jsp" // file 경로 지정
+    
+    // 잘 불러왔다면 true를 return
+    func insertItems(category: Int, useAge: Int, itemTitle: String, itemContent: String, itemImage: String, itemPrice: Int, userNickname: String, address: String, tag: String, item_userCode: Int) -> Bool{
+        var result: Bool = true
+        let urlAdd = "?category=\(category)&useAge=\(useAge)&itemTitle=\(itemTitle)&itemContent=\(itemContent)&itemImage=\(itemImage)&itemPrice=\(itemPrice)&userNickname=\(userNickname)&address=\(address)&tag=\(tag)&item_userCode=\(item_userCode)" // urlPath 뒤에 붙는 주소
+        urlPath = urlPath + urlAdd
+        print(urlPath)
+        
+        // 한글 url encoding
+        urlPath = urlPath.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        
+        let url: URL = URL(string: urlPath)!
+        // Session과 URL 연결
+        let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
+        // Task를 정의, Json의 전체를 가져오기 위함,
+        let task = defaultSession.dataTask(with: url){(data, response, error) in
+            if error != nil {
+                print("Failed to insert data")
+                result = false
+            }else {
+                print("Data is inserted")
+                result = true
+            }
+        }
+        task.resume() // task 실행
+        return result // 입력이 잘되면 true, 안되면 false를 return
+    }
+    
+}
+
+
---
+++ BabyProject/BabyProject/SA/ItemImageAddCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemImageAddCollectionViewCell.swift
+//  BabyProject
+//
+//  Created by TJ on 2021/08/02.
+//
+
+import UIKit
+
+class ItemImageAddCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var itemAddImageView: UIImageView!
+    
+    @IBOutlet weak var btnItemImageRemove: UIButton!
+    
+}
